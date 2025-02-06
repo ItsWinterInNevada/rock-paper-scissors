1cd0f07339e9113da7b73367ae0829ee952406ee
Ashley Winters <itswinterinnevada@mailbox.org>
2025-02-06 02:40:02 +0000

    Add input logic and use it to compare for winning condition

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
-const getComputerChoice = () => {
-    let computerChoice = Math.round(Math.random() * 100) / 100;
-    console.log(computerChoice);
+const getComputerChoice = (humanInput) => {
+  let computerChoice = Math.round(Math.random() * 100) / 100;
 
-    if (computerChoice <= 0.33) {
-        console.log(`You got Rock!`);
-    } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
-        console.log(`You got Paper!`);
-    } else {
-        console.log(`You got Scissors!`)
-    }
-}
+  if (computerChoice <= 0.33) {
+    computerChoice = `Rock`;
+    console.log(`You chose ${humanInput}. Computer chose ${computerChoice}.`);
+  } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
+    computerChoice = `Paper`;
+    console.log(`You chose ${humanInput}. Computer chose ${computerChoice}.`);
+  } else {
+    computerChoice = `Scissors`;
+    console.log(`You chose ${humanInput}. Computer chose ${computerChoice}.`);
+  }
 
-getComputerChoice()
+  if (
+    (computerChoice === `Rock` && humanInput === `Paper`) ||
+    (computerChoice === `Paper` && humanInput === `Scissors`) ||
+    (computerChoice === `Scissors` && humanInput === `Rock`)
+  ) {
+    console.log(`You Win!`);
+  } else if (computerChoice === humanInput) {
+    console.log(`You Draw!`);
+  } else {
+    console.log(`You Lose!`);
+  }
+};
+
+let getHumanChoice = () => {
+  let input = prompt(`Rock, Paper, or Scissors?`);
+
+  if (input === `Rock` || input === `Paper` || input === `Scissors`) {
+    return input;
+  } else {
+    return (input = prompt(
+      `Incorrect value entered! Rock, Paper, or Scissors? (Be sure to capitalise and check spelling!)`
+    ));
+  }
+};
+
+getComputerChoice(getHumanChoice());
