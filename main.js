fb039cb39921a2b0f39cf30b5c62a86169e9a153
Ashley Winters <itswinterinnevada@mailbox.org>
2025-02-06 01:44:26 +0000

    Add logic to generate computer answer to Rock, Paper, Scissors

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log(`Hello world!`);
+const getComputerChoice = () => {
+    let computerChoice = Math.round(Math.random() * 100) / 100;
+    console.log(computerChoice);
+
+    if (computerChoice <= 0.33) {
+        console.log(`You got Rock!`);
+    } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
+        console.log(`You got Paper!`);
+    } else {
+        console.log(`You got Scissors!`)
+    }
+}
+
+getComputerChoice()
